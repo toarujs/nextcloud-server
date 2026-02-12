ca3c2d161a46da8bf12f2dba1ef457d105e359fc
provokateurin <kate@provokateurin.de>
2026-02-12 12:05:11 +0100

    refactor(dav): Modernize Node
    Signed-off-by: provokateurin <kate@provokateurin.de>

--- build/rector-strict.php
+++ build/rector-strict.php
@@ -11,6 +11,7 @@ return (require __DIR__ . '/rector-shared.php')
 	->withPaths([
 		$nextcloudDir . '/build/rector-strict.php',
 		$nextcloudDir . '/core/BackgroundJobs/ExpirePreviewsJob.php',
+		$nextcloudDir . '/apps/dav/lib/Connector/Sabre/Node.php',
 	])
 	->withPreparedSets(
 		deadCode: true,
